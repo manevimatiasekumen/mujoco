822ef11e335f62f587c58e17c4f08b5f3d9d8ce1
manevimatiasekumen <manevimatias@creativa77.com.ar>
2025-10-22 16:47:05 -0300

    Update package structure

--- wasm/package/build.sh
+++ wasm/package/build.sh
@@ -28,8 +28,8 @@ echo "Building wasm bindings..."
 cmake --build build
 
 # 3. Now that 'wasm/dist/' is created, copy the artifacts
-#    into the 'wasm/package/' directory so npm can find them.
+#    into the 'wasm/package/dist/' directory so npm can find them.
 echo "Copying build artifacts from dist/ to package/..."
-cp dist/* package/
+cp dist/* package/dist/
 
 echo "--- Build and Copy Complete ---"
