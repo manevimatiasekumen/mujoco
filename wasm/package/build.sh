0b0ef8f5db6241c5dc0ffbc9804e773cf688a28f
manevimatiasekumen <manevimatias@creativa77.com.ar>
2025-10-24 15:49:43 -0300

    Remove echo lines

--- wasm/package/build.sh
+++ wasm/package/build.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
-echo "--- Starting MuJoCo WASM Build & Prep ---"
 export PATH="$(pwd)/node_modules/.bin:$PATH" && \
 emcmake cmake -S ../.. -B ../../build && \
 cmake --build ../../build && \
 emcmake cmake -S .. -B package/build && \
 cmake --build package/build
-echo "--- Build and Copy Complete ---"
