3a9d319af4405c6e14ccb2fb3d2f5945f10fc7fc
manevimatiasekumen <manevimatias@creativa77.com.ar>
2025-10-24 13:35:59 -0300

    Fix package builder script

--- wasm/package/build.sh
+++ wasm/package/build.sh
@@ -2,8 +2,8 @@
 set -e
 echo "--- Starting MuJoCo WASM Build & Prep ---"
 export PATH="$(pwd)/node_modules/.bin:$PATH" && \
-emcmake cmake -S .. -B ../build && \
-cmake --build ../build && \
-emcmake cmake -S . -B package/build && \
+emcmake cmake -S ../.. -B ../../build && \
+cmake --build ../../build && \
+emcmake cmake -S .. -B package/build && \
 cmake --build package/build
 echo "--- Build and Copy Complete ---"
